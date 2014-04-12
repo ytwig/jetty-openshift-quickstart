ee213b7395dbe0dfbd344fc686a71874482e52dc
Dan McPherson <dmcphers@redhat.com>
2014-04-12 13:18:11 -0400

    Change references to OPENSHIFT_*_LOG_DIR

--- start.sh
+++ start.sh
@@ -12,6 +12,6 @@ ln -s ${OPENSHIFT_REPO_DIR}deployments webapps
 
 CMD="java -Xms384m -Xmx412m -jar start.jar -Djetty.host=$OPENSHIFT_DIY_IP -Djetty.port=$OPENSHIFT_DIY_PORT"
 
-nohup $CMD > $OPENSHIFT_DIY_LOG_DIR/server.log 2>&1 &
+nohup $CMD > $OPENSHIFT_LOG_DIR/server.log 2>&1 &
 
 echo $! > jetty.pid

189ce99d0be8e6ed4e69e393312b4772d1c0d30a
ytwig <yuvaltwig@gmail.com>
2015-06-06 14:33:40 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ fi
 
 ln -s ${OPENSHIFT_REPO_DIR}deployments webapps
 
-CMD="java -Xms384m -Xmx412m -jar start.jar -Djetty.host=$OPENSHIFT_DIY_IP -Djetty.port=$OPENSHIFT_DIY_PORT"
+CMD="java -Xms384m -Xmx412m -jar start.jar -Djetty.host=$OPENSHIFT_DIY_IP -Djetty.port=$OPENSHIFT_DIY_PORT -Dspring.profiles.active=openshift"
 
 nohup $CMD > $OPENSHIFT_LOG_DIR/server.log 2>&1 &
 
